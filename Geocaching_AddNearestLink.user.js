4b25b0574d97631bd4379cd4446df32b0b6f85e0
Francois Crevola <francois.crevola@pmsipilot.com>
2014-12-02 11:40:23 +0100

    Add NearestLink script, Add DTstats script

---
+++ Geocaching_AddNearestLink.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Geocaching - Add nearest cache link
+// @namespace    http://www.crevola.org/francois/
+// @version      0.1
+// @description  Add a nearest cache link on cache page description
+// @author       Francois Crevola
+// @match        http://www.geocaching.com/geocache/GC*
+// @grant        none
+// ==/UserScript==
+
+// Add information about nb of D/T found
+var elts = $('#ctl00_ContentBody_lnkConversions');
+var url = elts[0].href;
+
+console.log(url);
+
+url = url.replace("wpt/?","seek/nearest.aspx?");
+url = url.replace("detail","f");
+
+console.log(url);
+
+url2 = url.replace("&f=1","");
+
+$('.LocationData').append('<div class="AlignRight"><a href="'+url2+'">Autres caches à proximité</a><br>(<a href="'+url+'">Non-trouvées par vous-même</a>)</div>');
+
+
---
+++ Geocaching_DTstats.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Geocaching Stats D/T
+// @namespace    http://www.crevola.org/francois/
+// @version      0.1
+// @description  Add info in D/T stats block
+// @author       Francois Crevola
+// @match        http://www.geocaching.com/my/statistics.aspx
+// @grant        none
+// ==/UserScript==
+
+// Add information about nb of D/T found
+var count = 0;
+var elements = $('#DifficultyTerrainCaches .stats_cell_many'); count += elements.length;
+elements = $('#DifficultyTerrainCaches .stats_cell_lots');     count += elements.length;
+elements = $('#DifficultyTerrainCaches .stats_cell_few');      count += elements.length;
+elements = $('#DifficultyTerrainCaches .stats_cell_some');     count += elements.length;
+elements = $('#DifficultyTerrainCaches .stats_cell_couple');   count += elements.length;
+var percent = (count*100)/81;
+$('#DifficultyTerrainCaches').append('<div class="totalDT" style="text-align:right">'+count+' / 81 ('+(percent.toPrecision(2))+ '%)</div>');
+
+// Add link to ProjectGC matrixDT tool
+$('#DifficultyTerrainCaches').append('<div class="matrixToolLink" style="text-align:right"><a href="http://project-gc.com/Maps/MapMatrix">Matrix DT Tool</a></div>');
+
+// Remove Lab Cache Disclaimer
+$(".labcache-disclaimer").remove();
+
