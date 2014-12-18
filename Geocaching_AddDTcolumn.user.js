6d6bbf82a9d194928433bbf71059f752d968a308
François Crevola <Krevo@users.noreply.github.com>
2014-12-18 08:58:38 +0100

    clean, remove debug code, indent

--- Geocaching_AddDTcolumn.user.js
+++ Geocaching_AddDTcolumn.user.js
@@ -11,18 +11,18 @@
 var resultDifficultyTerrainCaches = "";
 
 GM_xmlhttpRequest({
-  method: "GET",
-  url: "http://www.geocaching.com/my/statistics.aspx",
-  onload: function(response) {
+    method: "GET",
+    url: "http://www.geocaching.com/my/statistics.aspx",
+    onload: function(response) {
 	obj = $.parseHTML(response.responseText);
-      resultDifficultyTerrainCaches = $(obj).find("#DifficultyTerrainCaches");
-  }
+        resultDifficultyTerrainCaches = $(obj).find("#DifficultyTerrainCaches");
+    }
 });
 
 var elts = $(".SearchResultsTable td");
 
-$.each(elts, function( index, value ){
-	strValue = $(this).text();
+$.each(elts, function( index, value ) {
+    strValue = $(this).text();
     if (strValue.substr(0,2)=="GC") {
         var gccode = strValue;
         var knownDT = localStorage.getItem(gccode);
@@ -30,32 +30,32 @@ $.each(elts, function( index, value ){
         if (knownDT!=null) {
             data = knownDT;
         }
-		$(value).after('<td id="dt'+gccode+'" >'+data+'</td>');
+        $(value).after('<td id="dt'+gccode+'" >'+data+'</td>');
         url = "http://coord.info/"+gccode;
         GM_xmlhttpRequest({
-          method: "GET",
-          url: url,
-          onload: function(response) {
-            obj = $.parseHTML(response.responseText);
-              D = $(obj).find("#ctl00_ContentBody_uxLegendScale").html();
-              D = D.substring(D.indexOf("stars/stars")+11,D.indexOf(".gif"));
-              D = D.replace("_",".");
+            method: "GET",
+            url: url,
+            onload: function(response) {
+                obj = $.parseHTML(response.responseText);
+                D = $(obj).find("#ctl00_ContentBody_uxLegendScale").html();
+            	D = D.substring(D.indexOf("stars/stars")+11,D.indexOf(".gif"));
+            	D = D.replace("_",".");
 
-              T = $(obj).find("#ctl00_ContentBody_Localize12").html();
-              T = T.substring(T.indexOf("stars/stars")+11,T.indexOf(".gif"));
-              T = T.replace("_",".");
+            	T = $(obj).find("#ctl00_ContentBody_Localize12").html();
+            	T = T.substring(T.indexOf("stars/stars")+11,T.indexOf(".gif"));
+            	T = T.replace("_",".");
 
-              var nbDT = "0";
-              if (resultDifficultyTerrainCaches!=="") {
-                  nbDT = resultDifficultyTerrainCaches.find("#"+(((D-1)*2)+1)+"_"+(((T-1)*2)+1)).text();
-              }
-              var css = 'style="background-color: lightgreen"';
-              if (nbDT!="0") {
-                  css = 'style="text-decoration: line-through"';
-              }
-              localStorage.setItem(gccode,D+' / '+T);
-              $("#dt"+gccode).replaceWith('<td id="dt'+gccode+'" '+css+'>'+D+' / '+T+'</td>');
-          }
-        });
+            	var nbDT = "0";
+            	if (resultDifficultyTerrainCaches!=="") {
+	            nbDT = resultDifficultyTerrainCaches.find("#"+(((D-1)*2)+1)+"_"+(((T-1)*2)+1)).text();
+            	}
+                var css = 'style="background-color: lightgreen"';
+                if (nbDT!="0") {
+            	    css = 'style="text-decoration: line-through"';
+            	}
+            	localStorage.setItem(gccode,D+' / '+T);
+            	$("#dt"+gccode).replaceWith('<td id="dt'+gccode+'" '+css+'>'+D+' / '+T+'</td>');
+            }
+    	});
     }
 });
