42de7b06c7a358c3db708051c791dfd77fe878b7
François Crevola <Krevo@users.noreply.github.com>
2015-01-20 12:38:38 +0100

    Update for language handlinge

--- Geocaching_AddNearestLink.user.js
+++ Geocaching_AddNearestLink.user.js
@@ -1,14 +1,21 @@
 // ==UserScript==
 // @name         Geocaching - Add nearest cache link
-// @namespace    http://www.crevola.org/francois/
-// @version      0.1
+// @namespace    http://your.homepage/
+// @version      0.2
 // @description  Add a nearest cache link on cache page description
 // @author       Francois Crevola
 // @match        http://www.geocaching.com/geocache/GC*
-// @match        http://www.geocaching.com/seek/cache_details.aspx*
 // @grant        none
 // ==/UserScript==
 
+var lang = window.navigator.language;
+var label1 = 'Nearest cache';
+var label2 = 'Not already found by you';
+if (lang.substr(0,2)=="fr") {
+  label1 = 'Autres caches à proximité';
+  label2 = 'Non-trouvées par vous-même';
+}
+    
 // Add information about nb of D/T found
 var elts = $('#ctl00_ContentBody_lnkConversions');
 var url = elts[0].href;
@@ -18,4 +25,4 @@ url = url.replace("detail","f");
 
 url2 = url.replace("&f=1","");
 
-$('.LocationData').append('<div class="AlignRight"><a href="'+url2+'">Autres caches à proximité</a><br>(<a href="'+url+'">Non-trouvées par vous-même</a>)</div>');
+$('.LocationData').append('<div class="AlignRight"><a href="'+url2+'">'+label1+'</a><br>(<a href="'+url+'">'+label2+'</a>)</div>');
