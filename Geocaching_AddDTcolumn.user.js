76f8014d99a25c64f703bcab89310a7b7d79fe8b
François Crevola <francois@crevola.org>
2018-01-31 12:39:29 +0100

    Improve speed for already known caches

--- Geocaching_AddDTcolumn.user.js
+++ Geocaching_AddDTcolumn.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Geocaching - Add D/T Column
 // @namespace    http://www.crevola.org/francois/
-// @version      0.3
+// @version      0.4
 // @description  Add D/T column to recently viewed cache in profile default view
 // @author       Francois Crevola
 // @match        http*://www.geocaching.com/my/default.aspx
@@ -14,10 +14,29 @@ GM_xmlhttpRequest({
     method: "GET",
     url: "http://www.geocaching.com/my/statistics.aspx",
     onload: function(response) {
-	obj = $.parseHTML(response.responseText);
+        obj = $.parseHTML(response.responseText);
         resultDifficultyTerrainCaches = $(obj).find("#DifficultyTerrainCaches");
+        for (var D = 1; D<=5; D+=0.5) {
+            for (var T = 1; T<=5; T+=0.5) {
+                var nbDT = resultDifficultyTerrainCaches.find("#"+(((D-1)*2)+1)+"_"+(((T-1)*2)+1)).text();
+                localStorage.setItem(D+' / '+T, nbDT); // Refresh known nb of found for this D/T
+            }
+        }
+        console.log("Stats parsed !");
     }
 });
+
+function cssFromNbDt(nbDT) {
+    var css = '';
+    if (nbDT != null) {
+        if (nbDT>0) {
+            css = 'style="text-decoration: line-through"';
+        } else if  (nbDT==0) {
+            css = 'style="background-color: lightgreen"';
+        }
+    }
+    return css;
+}
 
 var elts = $(".SearchResultsTable td");
 
@@ -27,35 +46,34 @@ $.each(elts, function( index, value ) {
         var gccode = strValue;
         var knownDT = localStorage.getItem(gccode);
         var data = ".. / ..";
-        if (knownDT!=null) {
+        var css = '';
+        if (knownDT != null) {
             data = knownDT;
+            var nbDT = localStorage.getItem(knownDT);
+            css = cssFromNbDt(nbDT);
         }
-        $(value).after('<td id="dt'+gccode+'" >'+data+'</td>');
+        $(value).after('<td id="dt'+gccode+'" '+css+'>'+data+'</td>');
         url = "http://coord.info/"+gccode;
         GM_xmlhttpRequest({
             method: "GET",
             url: url,
             onload: function(response) {
+                console.log("Info for GC"+gccode+" loaded !");
                 obj = $.parseHTML(response.responseText);
                 D = $(obj).find("#ctl00_ContentBody_uxLegendScale").html();
-            	D = D.substring(D.indexOf("stars/stars")+11,D.indexOf(".gif"));
-            	D = D.replace("_",".");
+                D = D.substring(D.indexOf("stars/stars")+11,D.indexOf(".gif"));
+                D = D.replace("_",".");
 
-            	T = $(obj).find("#ctl00_ContentBody_Localize12").html();
-            	T = T.substring(T.indexOf("stars/stars")+11,T.indexOf(".gif"));
-            	T = T.replace("_",".");
+                T = $(obj).find("#ctl00_ContentBody_Localize12").html();
+                T = T.substring(T.indexOf("stars/stars")+11,T.indexOf(".gif"));
+                T = T.replace("_",".");
 
-            	var nbDT = "0";
-            	if (resultDifficultyTerrainCaches!=="") {
-	            nbDT = resultDifficultyTerrainCaches.find("#"+(((D-1)*2)+1)+"_"+(((T-1)*2)+1)).text();
-            	}
-                var css = 'style="background-color: lightgreen"';
-                if (nbDT!="0") {
-            	    css = 'style="text-decoration: line-through"';
-            	}
-            	localStorage.setItem(gccode,D+' / '+T);
-            	$("#dt"+gccode).replaceWith('<td id="dt'+gccode+'" '+css+'>'+D+' / '+T+'</td>');
+                localStorage.setItem(gccode,D+' / '+T); // Refresh the known D/T for this cache
+
+                var nbDT = localStorage.getItem(D+' / '+T);
+                css = cssFromNbDt(nbDT);
+                $("#dt"+gccode).replaceWith('<td id="dt'+gccode+'" '+css+'>'+D+' / '+T+'</td>');
             }
-    	});
+        });
     }
 });
